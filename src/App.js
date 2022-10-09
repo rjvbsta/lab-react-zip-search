42775deeae59fcb5cc58dc4ffea25376e2864179
Rajeev Basanta <rajeev.basanta@gmail.com>
2022-10-09 17:49:13 -0400

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-function City(props) {
-  return <div>This is the City component</div>;
+function City({state, estPop, city}) {
+  return <div>
+    {city}, {state}
+  </div>;
 }
 
-function ZipSearchField(props) {
-  return <div>This is the ZipSearchField component</div>;
+function ZipSearchField({handleChange}) {
+  return <div>
+    <label>
+      Zip Code:
+      <input type="text" name="name" onChange={handleChange}/>
+    </label>
+</div>;
 }
 
 function App() {
+
+  const [zipCode, setZipCode] = useState(null)
+
+  const[zipResults, setZipResults] = useState([])
+
+  const handleChange = (event) => {
+    const zipInput = event.target.value
+    setZipCode(zipInput)
+
+    if( zipInput.length === 5 ) {
+      fetch('https://ctp-zip-api.herokuapp.com/zip/'+zipInput)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .then((data) => setZipResults(data))
+    }
+    else {
+      setZipResults([])
+    }
+
+    
+  }
   return (
     <div className="App">
       <div className="App-header">
         <h1>Zip Code Search</h1>
       </div>
+      <br></br>
       <div className="mx-auto" style={{ maxWidth: 400 }}>
-        <ZipSearchField />
+        <ZipSearchField handleChange={handleChange}/>
         <div>
-          <City />
-          <City />
+          <City 
+            state = {zipResults.State}
+            estPop = {zipResults.EstimatedPopulation}
+            city = {zipResults.City}
+          />
         </div>
       </div>
     </div>
